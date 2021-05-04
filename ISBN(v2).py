eecc9c39b5eeda37d19c86fe1388f1fa27e54e56
alex <alexanderjcoates@gmail.com>
2021-05-04 15:40:30 +0100

    testing

--- ISBN(v2).py
+++ ISBN(v2).py
@@ -11,12 +11,12 @@ digit9 = 6
 digit10 = 1
 digit11 = 5
 digit12 = (
-( digit0 + (3 * digit1) + 
+(digit0 + (3 * digit1) + 
 digit2 + (3 * digit3) + 
 digit4 + (3 * digit5) + 
 digit6 + (3 * digit7) + 
 digit8 + (3 * digit9) + 
-digit10 + (3 * digit11) ) % 10)
+digit10 + (3 * digit11)) % 10)
 
 isbn_number = (digit0 + digit1 + digit2 + "-" + digit3 + "-" + digit4 + digit5 + digit6 + "-" + digit7 + digit8 + digit9 + digit10 + digit11 + "-" + digit12)
 print(isbn_number)
